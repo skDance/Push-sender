b601f4e67c4b59a00cb3f80bed37ca94ec286b94
Kirill Salov <steam-moi-v1.3@yandex.ru>
2023-01-29 22:18:27 +0400

    initial commit

---
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,44 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "LIKE")
+        .putData(
+            "content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    val newPostPush = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData(
+            "content", """ {
+            "userName" : "Netology",
+            "postText" : "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb"
+        }""".trimIndent()
+        )
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+    FirebaseMessaging.getInstance().send(newPostPush)
+}
